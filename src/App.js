3f8154776a39cd68c0cc75d8c3d4128ddbd1e17d
binay.karali <binay.karali@concur.com>
2020-05-28 01:48:11 +0530

    Callback Handlers

--- src/App.js
+++ src/App.js
@@ -2,49 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React'
-};
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
-  const handleChange = event =>{
-    console.log(event.target.value);
-  }
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(function(story) {
+    return story.title
+    .toLowerCase()
+    .includes(searchTerm.toLowerCase());
+  });
+
   return (
       <div>
-        <h1>
-          {welcome.greeting} {welcome.title}
-        </h1>
-        <label htmlFor="search"> Search: </label>
-        <input id = "search" type = "text" onChange={handleChange}/>
+        <h1>My Hacker Stories</h1>
+        <Search onSearch={handleSearch}/>
+
         <hr />
-        <List/>
+        <List list={searchedStories}/>
       </div>
-    )
+    );
 }
 
-const List = () =>
-  list.map((item) => (
+const List = (props) =>
+  props.list.map((item) => (
       <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title} </a>
@@ -55,5 +56,15 @@ const List = () =>
       </div>
     )
 );
+
+const Search = (props) => {
+
+  return (
+    <div>
+      <label htmlFor="search"> Search: </label>
+      <input id = "search" type = "text" onChange={props.onSearch}/>
+    </div>
+  );
+};
 
 export default App;
