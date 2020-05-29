5cd82c01607bbc3d9172b7dc751a5b954408e5c1
binay.karali <binay.karali@concur.com>
2020-05-29 22:35:07 +0530

    Third party library axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 const initialStories = [
   {
@@ -92,15 +93,13 @@ const App = () => {
   );
 
   const handleFetchStories = React.useCallback(() => {
-    if(!searchTerm) return;
 
     dispatchStories({type:'STORIES_FETCH_INIT'});
-    fetch(url)
-      .then(response => response.json())
+    axios.get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
     .catch(() =>
